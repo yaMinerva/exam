d2940589115452df4b950ad990dec046253369c5
yaMinerva <108409428+yaMinerva@users.noreply.github.com>
2022-06-29 16:11:49 +0300

    git.bat commit

---
+++ gitpush.bat
@@ -0,0 +1,7 @@
+git.exe init
+git.exe add . 
+git.exe commit -a -m "git.bat commit"
+git.exe branch -M main
+git.exe remote add origin https://github.com/yaMinerva/exam.git
+git.exe push -u origin main
+pause
